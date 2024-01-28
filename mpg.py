b19b1280718e222317d44b3515a9845cda033a46
iridianarreola <130425996+iridianarreola@users.noreply.github.com>
2024-01-27 23:14:43 -0700

    Add files via upload

--- mpg.py
+++ mpg.py
@@ -4,25 +4,36 @@
 print("The Miles Per Gallon program")
 print()
 
-# get input from the user
-miles_driven= float(input("Enter miles driven:\t\t"))
-gallons_used = float(input("Enter gallons of gas used:\t"))
-cost_per_gallon = float(input("Enter cost per gallon:\t"))
+another_trip = "y"
 
-# calculate miles per gallon
-mpg = miles_driven / gallons_used
-mpg = round(mpg, 1)
-total_gas_cost = gallons_used * cost_per_gallon
-total_gas_cost = round(total_gas_cost, 1)
-cost_per_mile = total_gas_cost / miles_driven
-cost_per_mile = round(cost_per_mile, 1)
-            
-# format and display the result
-print()
-print(f"Miles Per Gallon:\t\t{mpg}"
-      f"\nTotal Gas Cost:\t\t\t{total_gas_cost}"
-      f"\nCost Per Mile:\t\t\t{cost_per_mile}")
-print()
-print("Bye!") 
+while another_trip == "y":
+
+    # get input from the user
+    miles_driven = float(input("Enter miles driven:         "))
+    gallons_used = float(input("Enter gallons of gas used:  "))
+    cost_per_gallon = float(input("Enter cost per gallon:      "))
+    
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+    elif cost_per_gallon <= 0:
+        print("Cost per gallon must be greater than zero. Please try again.")
+    else:
+        # calculate and display
+        mpg = round(miles_driven / gallons_used, 2)
+        total_gas_cost = round(gallons_used * cost_per_gallon, 1)
+        cost_per_mile = round(total_gas_cost / miles_driven, 1)
+        print()
+        print("Miles Per Gallon:          ", mpg)
+        print("Total Gas Cost:            ", total_gas_cost)
+        print("Cost Per Mile:             ", cost_per_mile)
+        print()
+
+    another_trip = input("Get entries for another trip (y/n)? ")
+    print()
+
+print("Bye!")
+
 
 
